28d2d6f5f3e73a5fd02f36865e669b5ccb0517d4
Tabokaaah <139571300+Mansoryq@users.noreply.github.com>
2026-03-12 21:38:42 +0500

    Fix Supabase anon key

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Supabase configuration
 const SUPABASE_URL = 'https://qyobkvllhbtzhtewxzmr.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_hRjmXPKBeLjZhClghkV_Cg_PyBwcCf9';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5b2JrdmxsaGJ0emh0ZXd4em1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMjg0NTgsImV4cCI6MjA4ODkwNDQ1OH0.Cd5W7cvz9s6dYbwxFK6jMmyXlGwcReWa5N0gyQ6dhs8';
 
 const { createClient } = supabase;
 const _supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
